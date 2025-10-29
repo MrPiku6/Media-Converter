508df2d8962c2c3263e4b15b6aaec7215a4f422a
MrPiku6 <mrpiku323@gmail.com>
2025-10-29 22:01:42 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  conversionCount: {
+    type: Number,
+    default: 0,
+  },
+  lastConversionDate: {
+    type: Date,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
